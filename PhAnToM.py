950ff3c329d9dfd840a08856c95cc2737b086f95
TorbjornBak <s203555@dtu.dk>
2023-02-22 14:34:40 +0100

    Added more stuff

--- PhAnToM.py
+++ PhAnToM.py
@@ -3,13 +3,26 @@
 import subprocess, sys
 from argparse import ArgumentParser
 
+#Command line arguments
 
 parser = ArgumentParser(description="Help me!")
-
-#Test for subsampling
-parser.add_argument(["-ss","-subsampling"], action="store", dest="subsampling", type=float,default = 1,help = "Subsampling percentage")
-
+parser.add_argument(["-i","input"], action="store", dest="sraAccNr", type = str, help ="Input valid accesion number from Sequence Read Archive")
 args = parser.parse_args()
 
 if len(sys.argv) < 2:
     parser.print_help()
+    sys.exit()
+
+#Creates results directory for the pipeline results
+def directory_maker(sraAccNr):
+    global parent_directory
+    parent_directory = "PhAnTom" + sraAccNr
+    if not os.path.exists(parent_directory): 
+        subprocess.run(["mkdir",parent_directory])
+#Downloads sequence reads from SRA using fasterq-dump
+def sra_get(sraAccNr,directory):
+    print("Downloading from SRA using fasterq-dump")
+    subprocess.run(["fasterq-dump", sraAccNr, "--progress"], cwd = directory)
+
+def eukaryote_removal():
+    
