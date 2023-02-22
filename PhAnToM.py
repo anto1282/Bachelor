ed81276883a71bfb68325091ee64cb4dc44c5d91
TorbjornBak <s203555@dtu.dk>
2023-02-22 14:21:09 +0100

    Added argparser

--- PhAnToM.py
+++ PhAnToM.py
@@ -4,7 +4,6 @@ import subprocess, sys
 from argparse import ArgumentParser
 
 
-<<<<<<< HEAD
 parser = ArgumentParser(description="Help me!")
 
 #Test for subsampling
@@ -14,5 +13,3 @@ args = parser.parse_args()
 
 if len(sys.argv) < 2:
     parser.print_help()
-=======
->>>>>>> 81c824349f520fea3e0a4cc06fd7a0526c103743
