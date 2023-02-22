bdbb943fa22c45184c8cfd4988bac4f911042e53
TorbjornBak <s203555@dtu.dk>
2023-02-22 14:55:18 +0100

    added main

--- PhAnToM.py
+++ PhAnToM.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
 
-import subprocess, sys
+import subprocess, sys, os
 from argparse import ArgumentParser
 
 #Command line arguments
 
 parser = ArgumentParser(description="Help me!")
-parser.add_argument(["-i","input"], action="store", dest="sraAccNr", type = str, help ="Input valid accesion number from Sequence Read Archive")
+parser.add_argument(["-i","--input"], action="store", dest="sraAccNr", type = str, help ="Input valid accesion number from Sequence Read Archive")
 args = parser.parse_args()
-
-if len(sys.argv) < 2:
-    parser.print_help()
-    sys.exit()
 
 #Creates results directory for the pipeline results
 def directory_maker(sraAccNr):
-    global parent_directory
     parent_directory = "PhAnTom" + sraAccNr
     if not os.path.exists(parent_directory): 
         subprocess.run(["mkdir",parent_directory])
+    return parent_directory
+
 #Downloads sequence reads from SRA using fasterq-dump
 def sra_get(sraAccNr,directory):
     print("Downloading from SRA using fasterq-dump")
     subprocess.run(["fasterq-dump", sraAccNr, "--progress"], cwd = directory)
 
-def eukaryote_removal():
-    
+def main():
+    if len(sys.argv) < 2:
+        parser.print_help()
+        sys.exit()
+    sraAccNr = args.sraAccNr 
+    parent_directory = directory_maker(sraAccNr)
+    sra_get(sraAccNr,parent_directory)
