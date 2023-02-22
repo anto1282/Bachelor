a6e6b94bafc45d2d9308cbd64a96fc1c05229f69
TorbjornBak <s203555@dtu.dk>
2023-02-22 22:14:32 +0100

    Hey

--- PhAnToM.py
+++ PhAnToM.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 
-import subprocess, sys, os
+import subprocess, sys, os, multiprocessing
 from argparse import ArgumentParser
+import Assembly
+
 
 #Command line arguments
 
 parser = ArgumentParser(description="Help me!")
-parser.add_argument(["-i","--input"], action="store", dest="sraAccNr", type = str, help ="Input valid accesion number from Sequence Read Archive")
+parser.add_argument(["-i","--input"], action="store", dest = "sraAccNr", type = str, help ="Input valid accesion number from Sequence Read Archive.")
+parser.add_argument(["--flag"], action = "store", dest = "whatSPADES", type = str, default = "--meta", help = "Runs spades with the specified flag.")
+parser.add_argument(["--threads","-t"],action = "store", dest = "threads", type = int, default = multiprocessing.cpu_count(),help = "Specifies the total number of threads use in various of the programs in the pipeline.")
 args = parser.parse_args()
 
 #Creates results directory for the pipeline results
@@ -22,9 +26,21 @@ def sra_get(sraAccNr,directory):
     subprocess.run(["fasterq-dump", sraAccNr, "--progress"], cwd = directory)
 
 def main():
+
     if len(sys.argv) < 2:
         parser.print_help()
         sys.exit()
     sraAccNr = args.sraAccNr 
     parent_directory = directory_maker(sraAccNr)
     sra_get(sraAccNr,parent_directory)
+
+
+    assemblydirectory = Assembly.SPADES(read1,read2,parent_directory,args.whatSPADES)
+    Assembly.N50(parent_directory,assemblydirectory)
+    Assembly.PHAROKKA()
+
+
+
+### The end ###
+print("Pharokka.py finished running.")
+print("\nThanks for using the PhAnTomic's pipeline :-P")
