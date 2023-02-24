61332e4eb141e117f47fdb44b489b7d60ab828e7
anto1282 <s203557@dtu.dk>
2023-02-24 10:01:35 +0100

    Trimton

--- Trimton.py
+++ Trimton.py
@@ -20,10 +20,10 @@ def trimming(sraACCnr, directory, read1,read2, refFile):
     if not os.path.exists(directory + "/trimmed"):
         subprocess.run(["mkdir","trimmed"], cwd = directory) 
     if args.refFile:
-        subprocess.run(["conda", "run", "-n", "QC","bbduk.sh","-in=" + acc_nr_1,  "-in2=" + acc_nr_2, "-out=" + acc_nr_1_trimmed, "-out2=" + acc_nr_2_trimmed, "ref=" + refFile , "forcetrimleft=15" , "minbasequality=30"], cwd =directory)
+        subprocess.run(["mamba", "run", "-n", "QC","bbduk.sh","-in=" + acc_nr_1,  "-in2=" + acc_nr_2, "-out=" + acc_nr_1_trimmed, "-out2=" + acc_nr_2_trimmed, "ref=" + refFile , "forcetrimleft=15" , "minbasequality=30"], cwd =directory)
         print("Trim finished.")
     else:
-        subprocess.run(["conda", "run", "-n", "QC","bbduk.sh","-in=%s" % acc_nr_1,  "-in2=%s" % acc_nr_2, "-out=%s" % acc_nr_1_trimmed, "-out2=%s" % acc_nr_2_trimmed,"forcetrimleft=15" , "minbasequality=30"], cwd =directory)
+        subprocess.run(["mamba", "run", "-n", "QC","bbduk.sh","-in=%s" % acc_nr_1,  "-in2=%s" % acc_nr_2, "-out=%s" % acc_nr_1_trimmed, "-out2=%s" % acc_nr_2_trimmed,"forcetrimleft=15" , "minbasequality=30"], cwd =directory)
         print("Trim finished.")
 
 
