5cf19989434affc99700c7a1d86439050cb33115
TorbjornBak <s203555@dtu.dk>
2023-03-03 15:22:28 +0100

    added nextflow pipeline

--- PhAnToM.py
+++ PhAnToM.py
@@ -116,7 +116,7 @@ def main():
 
     
     #Maybe this is not necessary?
-    Contigs_Trimmed = Assembly.contigTrimming(assemblydirectory, "contigs.fasta", minLength=200) #Filters off too short contigs
+    Contigs_Trimmed = Assembly.contigTrimming(assemblydirectory, "contigs.fasta", minLength=500) #Filters off too short contigs
 
     pathToDeepVirFinder = "../../DeepVirFinder"
     if "DeepVirFinder" not in args.skip:
