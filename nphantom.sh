fb4002d97fb2c7fa4a2449e7d30a28dabd1962dc
anto1282 <s203557@dtu.dk>
2023-03-15 11:22:20 +0100

    Mjolnir

--- nphantom.sh
+++ nphantom.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=BACHBOYS
-#SBATCH --output=/projects/mjolnir1/people/qvx631/BACHELORASSEMBLIES
-#SBATCH --error=/projects/mjolnir1/people/qvx631/BACHELORASSEMBLIES/errors
+#SBATCH --output=/projects/mjolnir1/people/zpx817/
+#SBATCH --error=/projects/mjolnir1/people/zpx817/
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --tasks-per-node=1
@@ -11,9 +11,10 @@
 #SBATCH --mail-type=begin
 #SBATCH --mail-type=end
 #SBATCH --mail-type=fail
-#SBATCH --mail-user=s203555@dtu.dk
+#SBATCH --mail-user=s203557@dtu.dk
 export NXF_CLUSTER_SEED=$(shuf -i 0-16777216 -n 1)
 
 module purge
 module load jdk/1.8.0_291 miniconda singularity/3.8.0 nextflow
-srun nextflow run NPhAnToM.nf -with-mpi
+srun nextflow run NPhAnToM.nf --IDS SRR13557385 -profile cluster -with-mpi
+module purge
