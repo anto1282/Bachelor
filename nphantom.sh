73e0c9b2f1110b7996e461e6e53b365279bad541
anto1282 <s203557@dtu.dk>
2023-03-15 16:32:55 +0100

    asd

--- nphantom.sh
+++ nphantom.sh
@@ -11,11 +11,11 @@
 #SBATCH --mail-type=end
 #SBATCH --mail-type=fail
 #SBATCH --mail-user=s203557@dtu.dk
-#export NXF_CLUSTER_SEED=$(shuf -i 0-16777216 -n 1)
+export NXF_CLUSTER_SEED=$(shuf -i 0-16777216 -n 1)
 
-# module purge
-# module load openjdk/11.0.0
-# module load miniconda singularity/3.8.0 nextflow
+module purge
+module load openjdk/11.0.0
+module load miniconda singularity/3.8.0 nextflow
 
 if [ $1 == "-r" ];
 then
