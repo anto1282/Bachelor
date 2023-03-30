1e1c01ad117d5a69bd90d66d5317eca5e9a846e3
TorbjornBak <s203555@dtu.dk>
2023-03-30 13:38:13 +0200

    Changed time in bashscript

--- TBRnphantom.sh
+++ TBRnphantom.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=4G
-#SBATCH --time=03:00:00
+#SBATCH --time=01:00:00
 #SBATCH --mail-type=begin
 #SBATCH --mail-type=end
 #SBATCH --mail-type=fail
