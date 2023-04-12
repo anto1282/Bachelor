3c1f9bd858cd8f5fa455e12e01159b5bdcede8ad
anto1282 <s203557@dtu.dk>
2023-04-12 14:50:10 +0200

    Yeet?

--- PHAROKKA.sh
+++ PHAROKKA.sh
@@ -15,4 +15,4 @@
 
 
 singularity pull --dir /maps/projects/mjolnir1/data/cache/nf-core/singularity docker://quay.io/biocontainers/pharokka:1.2.1--hdfd78af_0 
-singularity run /maps/projects/mjolnir1/data/cache/nf-core/singularity/pharokka_1.2.1--hdfd78af_0.sif pharokka.py -i /projects/mjolnir1/people/zpx817/PipeLineFolder/Bachelor/Results/SRR23446273/ViralContigs/SRR23446273_Vis/mjolnir1/people/zpx817/PipeLineFolder/Bachelor/PLSPHAROKKA -f -t 4 -d /projects/mjolnir1/apps/conda/pharokka-1.2.1/pharokka_v1.2.0_databases -g prodigal -m
+singularity run -B /projects/mjolnir1/people/zpx817/PipeLineFolder/Bachelor/Results/SRR23446273/ViralContigs/SRR23446273_ViralContigs.fasta,/projects/mjolnir1/people/zpx817/PipeLineFolder/Bachelor/PLSPHAROKKA,/projects/mjolnir1/apps/conda/pharokka-1.2.1/pharokka_v1.2.0_databases /maps/projects/mjolnir1/data/cache/nf-core/singularity/pharokka_1.2.1--hdfd78af_0.sif pharokka.py -i /projects/mjolnir1/people/zpx817/PipeLineFolder/Bachelor/Results/SRR23446273/ViralContigs/SRR23446273_ViralContigs.fasta -o $HOME/output -f -t 4 -d /projects/mjolnir1/apps/conda/pharokka-1.2.1/pharokka_v1.2.0_databases -g prodigal -m
