4dafa0e74b207c7ed65539ecff8b3954cd70bfac
anto1282 <s203557@dtu.dk>
2023-04-12 14:51:15 +0200

    asd

--- PHAROKKA.sh
+++ PHAROKKA.sh
@@ -16,3 +16,4 @@
 
 singularity pull --dir /maps/projects/mjolnir1/data/cache/nf-core/singularity docker://quay.io/biocontainers/pharokka:1.2.1--hdfd78af_0 
 singularity run -B /projects/mjolnir1/people/zpx817/PipeLineFolder/Bachelor/Results/SRR23446273/ViralContigs/SRR23446273_ViralContigs.fasta,/projects/mjolnir1/people/zpx817/PipeLineFolder/Bachelor/PLSPHAROKKA,/projects/mjolnir1/apps/conda/pharokka-1.2.1/pharokka_v1.2.0_databases /maps/projects/mjolnir1/data/cache/nf-core/singularity/pharokka_1.2.1--hdfd78af_0.sif pharokka.py -i /projects/mjolnir1/people/zpx817/PipeLineFolder/Bachelor/Results/SRR23446273/ViralContigs/SRR23446273_ViralContigs.fasta -o $HOME/output -f -t 4 -d /projects/mjolnir1/apps/conda/pharokka-1.2.1/pharokka_v1.2.0_databases -g prodigal -m
+mv $HOME/output /projects/mjolnir1/people/zpx817/PipeLineFolder/Bachelor/PLSPHAROKKA
