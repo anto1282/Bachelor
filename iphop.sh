7a632678167524bc6204f17869391727bdcba862
anto1282 <s203557@dtu.dk>
2023-04-17 15:22:07 +0200

    asd

--- iphop.sh
+++ iphop.sh
@@ -12,4 +12,6 @@
 #SBATCH --mail-user=s203557@dtu.dk
 
 conda activate /projects/mjolnir1/apps/conda/iphop-1.2.0
+env
+echo $PERL5LIB
 iphop predict --fa_file /projects/mjolnir1/people/zpx817/PipeLineFolder/Bachelor/Results/SRR23446273/ViralContigs/SRR23446273_ViralContigs.fasta --db_dir /maps/projects/mjolnir1/data/databases/iphop/20230317/Sept_2021_pub --out_dir /projects/mjolnir1/people/zpx817/PipeLineFolder/Bachelor/Results/SRR23446273/ViralContigs/iphop_prediction --num_threads 8
