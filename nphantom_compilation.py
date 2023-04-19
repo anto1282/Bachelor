4834b2c662404db72d20dd153609481cf370d0e4
TorbjornBak <s203555@dtu.dk>
2023-04-19 15:10:14 +0200

    Adding features

--- nphantom_compilation.py
+++ nphantom_compilation.py
@@ -14,7 +14,7 @@
 #Find the corresponding viruses in the iphop results
 #Add a picture of the assembled phage
 
-import sys
+import sys, os
 
 
 outputfilename = str(sys.argv[1])
@@ -56,28 +56,98 @@ print(iphopdict)
 html_template = '''
 <!DOCTYPE html>
 <html>
-<head>
-	<title>{}</title>
-</head>
-<body>
-	<h1>{}</h1>
-	<p>{}</p>
-    <h1>{}</h1>
-    <p>{}</p>
-    
-	
-</body>
+	<head>
+		<meta charset="UTF-8">
+		<title>{}</title>
+		<style>
+			body {{
+				font-family: Arial, sans-serif;
+			}}
+			.container {{
+				margin: 0 auto;
+				max-width: 800px;
+			}}
+			.tab {{
+				display: none;
+			}}
+			.tab.active {{
+				display: block;
+			}}
+			.tab-button {{
+				background-color: #f2f2f2;
+				border: none;
+				color: black;
+				padding: 10px;
+				font-size: 16px;
+				cursor: pointer;
+			}}
+			.tab-button.active {{
+				background-color: #ccc;
+			}}
+			.picture {{
+				max-width: 100%;
+			}}
+		</style>
+	</head>
+	<body>
+		<div class="container">
+			<h1>{}</h1>
+			<p><strong>Key:</strong> {}</p>
+			<p><strong>Host:</strong> {}</p>
+			<p><strong>Picture:</strong></p>
+			<img class="picture" src="{}" alt="Example Picture">
+			<p><strong>DNA:</strong> {}</p>
+			<p><strong>Contig:</strong> {}</p>
+			<pre>print("Hello, world!")</pre>
+			<div class="tab-buttons">
+				{}
+			</div>
+			{}
+			<a href="index.html">Back to Index</a>
+		</div>
+		<script>
+			var tabs = document.querySelectorAll('.tab');
+			var buttons = document.querySelectorAll('.tab-button');
+			buttons.forEach(function(button, index) {{
+				button.addEventListener('click', function() {{
+					tabs.forEach(function(tab) {{
+						tab.classList.remove('active');
+					}});
+					buttons.forEach(function(button) {{
+						button.classList.remove('active');
+					}});
+					tabs[index].classList.add('active');
+					buttons[index].classList.add('active');
+				}});
+			}});
+		</script>
+	</body>
 </html>
+
 '''
 
-#Creating a file for each phage with name, contig and genus
-with open(outputfilename,'w') as file:
-    for key in iphopdict:
+
+# Create a directory to store the HTML files
+if not os.path.exists('pages'):
+    os.makedirs('pages')
+
+# Generate an HTML file for each key
+for key in iphopdict:
+    outputfilename = os.path.join('pages', '{}.html'.format(key))
+    host = "Likely host: " + iphopdict[key]
+    DNA = "The DNA of the phage:\n"
+    contig = virusdict[key]
+    picture = "test.jpg"
+    html = html_template.format(outputfilename, key, host, picture, DNA, contig)
+    with open(outputfilename, 'w') as f:
+        f.write(html)
+
+# #Creating a file for each phage with name, contig and genus
+# with open(outputfilename,'w') as file:
+#     for key in iphopdict:
         
-        host = "Likely host: " + iphopdict[key]
-        DNA = "The DNA of the phage: "
-        contig = virusdict[key]
-        file.write(html_template.format(outputfilename,key,host, DNA, contig))
+        
+#         file.write(html_template.format(outputfilename,key,host, DNA, contig))
 
 
 
