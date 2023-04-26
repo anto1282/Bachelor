793df43e8ac7dc188196ce3e9b291964fd0c667a
anto1282 <s203557@dtu.dk>
2023-04-26 14:50:37 +0200

    NuVirkerDetRigtigMeget

--- nphantom_copy.sh
+++ nphantom_copy.sh
@@ -4,9 +4,9 @@
 #SBATCH --output=/projects/mjolnir1/people/zpx817/BachAssemblies
 #SBATCH --error=/projects/mjolnir1/people/zpx817/errors
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=1G
-#SBATCH --time=0:30:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=4G
+#SBATCH --time=5:00:00
 #SBATCH --mail-type=begin
 #SBATCH --mail-type=end
 #SBATCH --mail-type=fail
