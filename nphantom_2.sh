87b2f71fd960c0f210611dd822bd68517b152124
TorbjornBak <s203555@dtu.dk>
2023-04-29 11:11:34 +0200

    Created new bash script

---
+++ nphantom_2.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#SBATCH --job-name=NPhAnToM_%j
+#SBATCH --output=/projects/mjolnir1/people/%u/BachAssemblies_%j_%a
+#SBATCH --error=/projects/mjolnir1/people/%u/errors_%j_%a
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=4G
+#SBATCH --time=10:00:00
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=end
+#SBATCH --mail-type=fail
+
+# Parse arguments for slurm
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+        --mail-user)
+        mail_user="$2"
+        shift
+        shift
+        ;;
+        *)    # Unknown option
+        echo "Unknown option: $1"
+        exit 1
+        ;;
+    esac
+done
+
+# Check if mail-user argument was passed
+if [ -z "$mail_user" ]; then
+  echo "mail-user argument is required"
+  exit 1
+fi
+
+export SINGULARITY_LOCALCACHEDIR="/maps/projects/mjolnir1/people/${USER}/SingularityTMP"
+export SINGULARITY_TMPDIR="/maps/projects/mjolnir1/people/${USER}/SingulquarityTMP"
+
+
+
+export NXF_CLUSTER_SEED=$(shuf -i 0-16777216 -n 1)
+export NXF_CONDA_ENABLED=true
+
+module purge
+module load openjdk/11.0.0
+module load singularity/3.8.0 nextflow miniconda/4.11.0
+
+
+# srun nextflow run NPhAnToM.nf ${SRRNUMBER} -profile ${PROFILE} ${RESUME} -with-mpi -with-tower --accessToken ${TOWERTOKEN} --minLength ${MINLENGTH} --contigs ${CONTIGS}
+#srun nextflow run NPhAnToM.nf "$@"
+echo "$@"
