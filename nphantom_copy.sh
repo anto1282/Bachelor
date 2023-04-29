bc163867eab580021e24cb3c296866f091f2ddf4
TorbjornBak <s203555@dtu.dk>
2023-04-29 11:00:17 +0200

    Changed back to original version of nphantom_copy

--- nphantom_copy.sh
+++ nphantom_copy.sh
@@ -43,11 +43,6 @@ while [[ $# -gt 0 ]]; do
       RESUME=-resume
       shift
       ;;
-    --filename)
-      FILENAME="--pair_file_names $2"
-      shift
-      shift
-      ;;
     -*|--*)
       echo "Unknown option $1"
       exit 1
@@ -85,5 +80,5 @@ module load openjdk/11.0.0
 module load singularity/3.8.0 nextflow miniconda/4.11.0
 
 
-srun nextflow run NPhAnToM.nf ${SRRNUMBER} ${FILENAME} -profile ${PROFILE} ${RESUME} -with-mpi -with-tower --accessToken ${TOWERTOKEN} --minLength ${MINLENGTH} --contigs ${CONTIGS}
+srun nextflow run NPhAnToM.nf ${SRRNUMBER} -profile ${PROFILE} ${RESUME} -with-mpi -with-tower --accessToken ${TOWERTOKEN} --minLength ${MINLENGTH} --contigs ${CONTIGS}
 
