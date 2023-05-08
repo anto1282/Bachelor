77baddd8138c04330cb36634315fb1b060e4a531
TorbjornBak <s203555@dtu.dk>
2023-05-08 15:05:20 +0200

    Bug

--- virextractor.py
+++ virextractor.py
@@ -94,7 +94,7 @@ with open("vir_pred_file.txt",'w') as pred_file:
             Phager = True
         if phage in DVFset:
             DVF = True
-        pred_string = "\t".join(phage, str([Seeker,Phager,DVF].count(True)), str(Seeker), str(Phager), str(DVF))
+        pred_string = "\t".join([phage, str([Seeker,Phager,DVF].count(True)), str(Seeker), str(Phager), str(DVF)])
         pred_file.write()
 
 
