a29973581e02d06ad6b6ab334c6d2e7a3991b437
TorbjornBak <s203555@dtu.dk>
2023-05-11 14:44:57 +0200

    New script

---
+++ nphantom_interactive.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p /maps/projects/mjolnir1/people/${USER}/SingularityTMP
+export SINGULARITY_LOCALCACHEDIR="/maps/projects/mjolnir1/people/${USER}/SingularityTMP"
+export SINGULARITY_TMPDIR="/maps/projects/mjolnir1/people/${USER}/SingularityTMP"
+
+
+
+export NXF_CLUSTER_SEED=$(shuf -i 0-16777216 -n 1)
+export NXF_CONDA_ENABLED=true
+
+module purge
+module load openjdk/11.0.0
+module load singularity/3.8.0 nextflow miniconda/4.11.0
