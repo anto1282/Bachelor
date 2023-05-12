4fd86e105ff3e99696aca982de11a2ad003e518b
TorbjornBak <s203555@dtu.dk>
2023-05-12 09:09:58 +0200

    Changed versions of nextflow and ojdk

--- nphantom_2.sh
+++ nphantom_2.sh
@@ -22,8 +22,8 @@ export NXF_CLUSTER_SEED=$(shuf -i 0-16777216 -n 1)
 export NXF_CONDA_ENABLED=true
 
 module purge
-module load openjdk/20.0.0
-module load singularity/3.8.0 nextflow miniconda/4.11.0
+module load openjdk/17.0.3
+module load singularity/3.8.0 nextflow/22.10.4 miniconda/4.11.0
 
 
 # srun nextflow run NPhAnToM.nf ${SRRNUMBER} -profile ${PROFILE} ${RESUME} -with-mpi -with-tower --accessToken ${TOWERTOKEN} --minLength ${MINLENGTH} --contigs ${CONTIGS}
