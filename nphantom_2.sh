a17a0d40f39cdd0d0d3cdc0919850298ccef8d92
TorbjornBak <114398738+TorbjornBak@users.noreply.github.com>
2023-05-12 13:58:31 +0200

    Update nphantom_2.sh

--- nphantom_2.sh
+++ nphantom_2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=NPhAnToM_%j
-#SBATCH --output=/projects/mjolnir1/people/%u/nextflowout/BachAssemblies_%j_%a
-#SBATCH --error=/projects/mjolnir1/people/%u/nextflowout/errors_%j_%a
+#SBATCH --output=/projects/mjolnir1/people/%u/nextflowout/stdout_%j
+#SBATCH --error=/projects/mjolnir1/people/%u/nextflowout/error_%j
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=4G
@@ -15,14 +15,14 @@
 export SINGULARITY_LOCALCACHEDIR="/maps/projects/mjolnir1/people/${USER}/SingularityTMP"
 export SINGULARITY_TMPDIR="/maps/projects/mjolnir1/people/${USER}/SingularityTMP"
 
-
-
 export NXF_CLUSTER_SEED=$(shuf -i 0-16777216 -n 1)
 export NXF_CONDA_ENABLED=true
 
+#export NXF_work="/maps/projects/mjolnir1/people/${USER}/"
+
 module purge
-module load openjdk/11.0.0
-module load singularity/3.8.0 nextflow miniconda/4.11.0
+module load openjdk/17.0.3
+module load singularity/3.8.0 nextflow/22.10.4 miniconda/4.11.0
 
 
 # srun nextflow run NPhAnToM.nf ${SRRNUMBER} -profile ${PROFILE} ${RESUME} -with-mpi -with-tower --accessToken ${TOWERTOKEN} --minLength ${MINLENGTH} --contigs ${CONTIGS}
