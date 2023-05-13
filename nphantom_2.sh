d11e2dad594aa0e4e6d5982f1bd6e2c1a78fe3d2
TorbjornBak <s203555@dtu.dk>
2023-05-13 10:07:10 +0200

    Changed amount of memory for sbatch script

--- nphantom_2.sh
+++ nphantom_2.sh
@@ -4,9 +4,9 @@
 #SBATCH --output=/projects/mjolnir1/people/%u/nextflowout/stdout_%j
 #SBATCH --error=/projects/mjolnir1/people/%u/nextflowout/error_%j
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=4G
-#SBATCH --time=10:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=8G
+#SBATCH --time=20:00:00
 #SBATCH --mail-type=begin
 #SBATCH --mail-type=end
 #SBATCH --mail-type=fail
