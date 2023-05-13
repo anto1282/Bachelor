411944b6b5fef8900d67402a451fa3202623e3f1
TorbjornBak <s203555@dtu.dk>
2023-05-13 10:20:40 +0200

    Bugfixing

--- nphantom_2.sh
+++ nphantom_2.sh
@@ -24,6 +24,7 @@ module purge
 module load openjdk/17.0.3
 module load singularity/3.8.0 nextflow/22.10.4 miniconda/4.11.0
 
+echo $NXF_work
 
 # srun nextflow run NPhAnToM.nf ${SRRNUMBER} -profile ${PROFILE} ${RESUME} -with-mpi -with-tower --accessToken ${TOWERTOKEN} --minLength ${MINLENGTH} --contigs ${CONTIGS}
 srun nextflow run NPhAnToM.nf $@ 
