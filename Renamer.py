a1666ce4ebfe29d70ecf5df60c3168b1aa73ee86
anto1282 <s203557@dtu.dk>
2023-05-18 10:19:04 +0200

    Renamer.py

---
+++ Renamer.py
@@ -0,0 +1,18 @@
+
+import sys
+import re
+
+InputName = sys.argv[1]
+
+Infile = open(InputName, "r")
+OutFile = open(InputName + "_renamed", "w")
+
+a = InputName[0:-19]
+
+for line in Infile:
+    if line.startswith(">"):
+        x = re.search("(>NODE_\d*_).*",line)
+
+        print(a+ x.group(1), file = OutFile)
+    else:
+        print(line, end= "", file = OutFile)
